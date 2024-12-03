d8021405618d3bfe20fc56ecc6969630e32e83b8
HuyPT3 <huypt3@rikkeisoft.com>
2024-12-03 14:09:00 +0700

    add logic minimize note view

--- TextMarkerEditor/Note/NoteView.swift
+++ TextMarkerEditor/Note/NoteView.swift
@@ -13,24 +13,43 @@ class NoteView: UIView {
     @IBOutlet weak var topView: UIView!
     @IBOutlet weak var dragView: UIView!
     @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var contentTextView: UIView!
+    @IBOutlet weak var noteImageView: UIImageView!
     var didOpenTextBox: (() -> Void)?
     
     private var initialSize: CGSize = .zero
     private var initialTouchPoint: CGPoint = .zero
+    private var isMinimized = false
+    private var originalFrame: CGRect = .zero
+    private var minimizedFrame: CGRect = .zero
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        originalFrame = frame
         loadXib()
         addGestures()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-//        loadXib()
     }
     
     @IBAction func didTapMinimize(_ sender: Any) {
+        if isMinimized {
+            return
+        }
+        topView.isHidden = true
+        contentLabel.isHidden = true
+        dragView.isHidden = true
+        noteImageView.isHidden = false
+        minimizedFrame = CGRect(x: originalFrame.origin.x, y: originalFrame.origin.y, width: 100, height: 200)
         
+        UIView.animate(withDuration: 0.5, animations: {
+            self.frame = self.minimizedFrame
+            self.layer.cornerRadius = self.frame.height / 2
+        }) { _ in
+            self.isMinimized = true
+        }
     }
     
     func loadXib() {
@@ -40,27 +59,12 @@ class NoteView: UIView {
     }
     
     private func addGestures() {
-        // Pan gesture for moving the view
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
-        self.topView.addGestureRecognizer(panGesture)
         
-        // Tap gesture for text input
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        self.contentView.addGestureRecognizer(tapGesture)
+        self.contentTextView.addGestureRecognizer(tapGesture)
         
-        // Pan gesture for resizing (on the resize handle only)
         let resizeGesture = UIPanGestureRecognizer(target: self, action: #selector(handleResize(_:)))
         dragView.addGestureRecognizer(resizeGesture)
-    }
-    
-    @objc private func handlePan(_ gesture: UIPanGestureRecognizer) {
-        let translation = gesture.translation(in: self.superview)
-        self.contentView.center = CGPoint(x: contentView.center.x + translation.x, y: contentView.center.y + translation.y)
-
-//        if let view = gesture.view {
-//            view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
-//        }
-        gesture.setTranslation(.zero, in: self.superview)
     }
     
     @objc private func handleResize(_ gesture: UIPanGestureRecognizer) {
@@ -72,29 +76,27 @@ class NoteView: UIView {
         } else if gesture.state == .changed {
             let deltaX = location.x - initialTouchPoint.x
             let deltaY = location.y - initialTouchPoint.y
-            
-            let newWidth = max(50, initialSize.width + deltaX)
-            let newHeight = max(50, initialSize.height + deltaY)
+            let newWidth = max(200, initialSize.width + deltaX)
+            let newHeight = max(300, initialSize.height + deltaY)
             
             self.frame.size = CGSize(width: newWidth, height: newHeight)
         }
     }
     
     @objc private func handleTap(_ gesture: UITapGestureRecognizer) {
-        didOpenTextBox?()
-//        textField.isHidden = false
-//        textField.becomeFirstResponder()
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        textField.isHidden = true
-        return true
-    }
-}
-
-extension NSObject {
-    static var className: String {
-        String(describing: self)
+        if isMinimized {
+            topView.isHidden = false
+            contentLabel.isHidden = false
+            dragView.isHidden = false
+            noteImageView.isHidden = true
+            UIView.animate(withDuration: 0.5, animations: {
+                self.frame = self.originalFrame
+            }) { _ in
+                self.isMinimized = false
+            }
+        } else {
+            didOpenTextBox?()
+        }
+        
     }
 }
